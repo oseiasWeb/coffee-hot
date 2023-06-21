d38216a5d31b3d5b7c7fbd4947a2528567e3a860
oseiasWeb <oseias.n.m@gmail.com>
2023-06-21 15:28:05 -0300

    Aplicada a animação da pesquisa, compra e menu via JS

--- js/script.js
+++ js/script.js
@@ -3,3 +3,15 @@ let navbar = document.querySelector('.navbar');
 document.querySelector('#menu-btn').onclick = () => {
     navbar.classList.toggle('active');
 }
+
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () => {
+    searchForm.classList.toggle('active');
+}
+
+let cartItem = document.querySelector('.cart-items-container');
+
+document.querySelector('#cart-btn').onclick = () => {
+    cartItem.classList.toggle('active');
+}
